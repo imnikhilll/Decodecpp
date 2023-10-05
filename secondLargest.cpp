cef2f9e0fa18bcb26dc8300e0034c52748e2dcee
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2023-10-06 01:23:55 +0530

    Add files via upload
    Second largest element in array

---
+++ secondLargest.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    int n;
+
+    cout<<"Enter the size of array : ";
+
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<=n-1;i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    for(int i=1;i<=n-1;i++){
+        if(arr[i]>max) max=arr[i];
+    }
+
+    int smax= INT_MIN;
+    
+
+    
+
+
+    
+}
