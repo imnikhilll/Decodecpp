a976042098a53f7a0a0d113e6599f13b13819d6f
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2023-10-16 23:58:26 +0530

    reversing the string

---
+++ string.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+
+void reverseWords(string s)
+{
+
+	
+	vector<string> tmp;
+	string str = "";
+	for (int i = 0; i < s.length(); i++) {
+
+		if (s[i] == ' ') {
+			tmp.push_back(str);
+			str = "";
+		}
+
+		
+		else
+			str += s[i];
+	}
+
+
+	tmp.push_back(str);
+
+	int i;
+	for (i = tmp.size() - 1; i > 0; i--)
+		cout << tmp[i] << " ";
+	// Last word remaining,print it
+	cout << tmp[0] << endl;
+}
+
+
+int main()
+{
+	string s = "i like this program very much";
+	reverseWords(s);
+	return 0;
+}
