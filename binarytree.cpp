2282a0e7d4a2a493e8c8315b607244003783b665
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2023-11-26 23:05:40 +0530

    tree

---
+++ binarytree.cpp
@@ -0,0 +1,25 @@
+//WAP to traverse the elements of the graph using bfs
+
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float n;
+    cin>>n;
+    float a;
+    cin>>a;
+    float sum;
+    sum=a+n;
+    
+    cout<<setprecision(2);
+    cout<<sum;
+    
+    
+
+
+return 0;
+}
