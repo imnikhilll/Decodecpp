81a7ed0a043c3562c702c6d4f17ec8bae1769b0c
imnikhilll <146981399+imnikhilll@users.noreply.github.com>
2023-11-27 23:18:36 +0530

    graph

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,51 +1,27 @@
-// C++ program for implementation of
-// selection sort
-#include <bits/stdc++.h>
+//SELECTION SORTING
+#include <iostream>
 using namespace std;
+int main(){
+    int a[10],n,i,j,loc,small,temp;
+    cout<<"Enter the range";
+    cin>>n;
 
-// Function for Selection sort
-void selectionSort(int arr[], int n)
-{
-	int i, j, min_idx;
-     
-	for (i = 0; i < n - 1; i++) {
-
-		// Find the minimum element in
-		// unsorted array
-		min_idx = i;
-		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[min_idx])
-				min_idx = j;
-		}
-
-		// Swap the found minimum element
-		// with the first element
-		if (min_idx != i)
-			swap(arr[min_idx], arr[i]);
-	}
+    cout<<"Enter the Elements";
+    for(i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(i=0;i<n;i++){
+        small=a[i];
+        loc=i;
+        for(j=i+1;j<n;j++){
+            if (a[j]<small){
+                small=a[j];
+                loc=j;
+            }
+        }
+    }
+    temp=a[i];
+    a[i]=a[loc];
+    a[loc]=temp;
+    return 0;
 }
-
-// Function to print an array
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++) {
-		cout << arr[i] << " ";
-		cout << endl;
-	}
-}
-
-// Driver program
-int main()
-{
-	int arr[] = { 64, 25, 12, 22, 11 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-
-	// Function Call
-	selectionSort(arr, n);
-	cout << "Sorted array: \n";
-	printArray(arr, n);
-	return 0;
-}
-
-// This is code is contributed by nikhil
